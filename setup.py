09bac8b0be3bd2ae9b1473d1da89ca8290151a81
Swayam Patil <98048555+Swish78@users.noreply.github.com>
2023-06-25 15:40:30 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='strkitN',
     version='0.1.0',
-    author='Your Name',
-    author_email='your_email@example.com',
+    author='Swayam Patil',
+    author_email='swayampatil7803@gmail.com',
     description='A string manipulation package with additional features',
     long_description='''strkitN is a Python package that provides various string manipulation functions 
                         along with additional features such as palindrome detection, substring operations, 
                         and more.''',
-    url='https://github.com/yourusername/strkitN',
+    url='https://github.com/Swish78/strkitN',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
